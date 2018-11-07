f6b9ac5c6b42dca64ce085b704b8d5bc9e8dafbf
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-11-07 10:57:36 -0800

    update styles

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,4 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App';
 
-window.ProductCarousel = App;
-// ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('carousel-container'));
